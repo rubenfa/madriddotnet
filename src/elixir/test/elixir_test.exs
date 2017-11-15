3b4b336704d1d69548968f99fcd9b65e95d23bbb
rubenfa <ruben.fernandez.alfonso@gmail.com>
2017-11-15 10:01:52 +0100

    changes

--- src/elixir/test/elixir_test.exs
+++ src/elixir/test/elixir_test.exs
@@ -6,8 +6,10 @@ defmodule ElixirTest do
 
   alias MadridDotNet.Testing.Test
 
-  test "Se muestra un mensaje en pantalla cuando se suman valores no numéricos" do
-    assert capture_io(fn -> Test.sum("a", 1) end) == "La función sum, solo puede sumar números\n"
+  test "Mensaje en pantalla cuando se suman valores no numéricos" do
+    assert 
+      capture_io(fn -> Test.sum("a", 1) end) == 
+            "La función sum, solo puede sumar números\n"
   end
 
 end
